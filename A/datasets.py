6c1586de5e376d89975564488351e1a4dad9574b
Yusen Liu <yusen0406@foxmail.com>
2023-04-19 02:44:26 +0100

    environment

--- A/datasets.py
+++ A/datasets.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 from src.utils import ImageTransforms
-import torchvision.transforms as transforms
+
 
 class SRDataset(Dataset):
     """
@@ -93,70 +93,42 @@ class SRDataset(Dataset):
         return len(self.images)
     
 
-class SRCNNDataset(Dataset):
+    # class SRCNNDataset(Dataset):
         
-    def __init__(self, data_folder, split, desired_size, scaling_factor):
-        # super(SRCNNDataset, self).__init__()
-        self.data_folder = data_folder
-        self.split = split.lower()
-        self.desired_size = desired_size
-        self.scaling_factor = int(scaling_factor)
+    #     def __init__(self, image_dir, zoom_factor):
+    #         super(SRCNNDataset, self).__init__()
+    #         hr_images_list = []
+    #     if self.split == 'train':
+    #         hd = data_folder / 'DIV2K_train_HR'
+    #         for i in os.listdir(hd):
+    #             img_path = hd / str(i)
+    #             hr_images_list.append(img_path)
+    #         self.images = hr_images_list
+    #     else:
+    #         hd = data_folder / 'DIV2K_valid_HR'
+    #         for i in os.listdir(hd):
+    #             img_path = hd / str(i)
+    #             hr_images_list.append(img_path)
+    #         self.images = hr_images_list
+    #         self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
 
+    #         crop_size = CROP_SIZE - (CROP_SIZE % zoom_factor) # Valid crop size
+    #         self.input_transform = transforms.Compose([transforms.CenterCrop(crop_size), # cropping the image
+    #                                   transforms.Resize(crop_size//zoom_factor),  # subsampling the image (half size)
+    #                                   transforms.Resize(crop_size, interpolation=Image.BICUBIC),  # bicubic upsampling to get back the original size 
+    #                                   transforms.ToTensor()])
+    #         self.target_transform = transforms.Compose([transforms.CenterCrop(crop_size), # since it's the target, we keep its original quality
+    #                                    transforms.ToTensor()])
 
+    #     def __getitem__(self, index):
+    #         input = load_img(self.image_filenames[index])
+    #         target = input.copy()
+        
+    #         # input = input.filter(ImageFilter.GaussianBlur(1)) 
+    #         input = self.input_transform(input)
+    #         target = self.target_transform(target)
 
-        # Read list of image-paths
-        hr_images_list = []
-        if self.split == 'train':
-            hd = data_folder / 'DIV2K_train_HR'
-            for i in os.listdir(hd):
-                img_path = hd / str(i)
-                hr_images_list.append(img_path)
-            self.images = hr_images_list
-        else:
-            hd = data_folder / 'DIV2K_valid_HR'
-            for i in os.listdir(hd):
-                img_path = hd / str(i)
-                hr_images_list.append(img_path)
-            self.images = hr_images_list
+    #         return input, target
 
-        crop_size = self.desired_size
-            
-        self.input_transform = transforms.Compose([transforms.CenterCrop(crop_size), # cropping the image
-                                      transforms.Resize(crop_size//scaling_factor),  # subsampling the image (half size)
-                                      transforms.Resize(crop_size, transforms.InterpolationMode.BICUBIC),
-                                      transforms.ToTensor()])
-        self.target_transform = transforms.Compose([transforms.CenterCrop(crop_size), # since it's the target, we keep its original quality
-                                       transforms.ToTensor()])
-
-    def __getitem__(self, i):
-        """
-        This method is required to be defined for use in the PyTorch DataLoader.
-        :param i: index to retrieve
-        :return: the 'i'th pair LR and HR images to be fed into the model
-        """
-        # Read image       
-        img_hr_dir = self.images[i]
-        index = img_hr_dir.stem
-        if self.split == 'train':
-            img_lr_dir = self.data_folder /  f"DIV2K_train_LR_bicubic_X{self.scaling_factor}" / "DIV2K_train_LR_bicubic" / f"X{self.scaling_factor}"/ f'{index}x{self.scaling_factor}.png'
-        else:
-            img_lr_dir = self.data_folder /  f"DIV2K_valid_LR_bicubic_X{self.scaling_factor}" / "DIV2K_valid_LR_bicubic" / f"X{self.scaling_factor}"/ f'{index}x{self.scaling_factor}.png'
-
-
-        img_lr = Image.open(img_lr_dir).convert('YCbCr')
-        img_lr_y, _, _ = img_lr.split()
-        lr_img = self.input_transform(img_lr_y)
-        img_hr = Image.open(img_hr_dir).convert('YCbCr')
-        img_hr_y, _, _ = img_hr.split() 
-        hr_img = self.target_transform(img_hr_y)
-
-        return lr_img, hr_img
-
-
-    def __len__(self):
-        """
-        This method is required to be defined for use in the PyTorch DataLoader.
-
-        :return: size of this data (in number of images)
-        """
-        return len(self.images)
+    #     def __len__(self):
+    #         return len(self.image_filenames)
--- src/models.py
+++ src/models.py
@@ -340,5 +340,3 @@ class TruncatedVGG19(nn.Module):
         output = self.truncated_vgg19(input)  # (N, feature_map_channels, feature_map_w, feature_map_h)
 
         return output
-
-
